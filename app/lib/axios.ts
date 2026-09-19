f86ac0b60829ff508b5f7854bafda70ee613283b
Rick-jo07 <rickjo.coder@gmail.com>
2026-09-19 15:10:08 +0700

    added Interceptor

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -5,4 +5,18 @@ const api = axios.create({
     withCredentials: true, // Mengatur axios untuk mengirim cookie dengan setiap permintaan
 });
 
+api.interceptors.request.use((config) => {
+  // Coba ambil token dari cookie browser
+  const match = document.cookie.match(new RegExp('(^| )token=([^;]+)'));
+  const token = match ? match[2] : null;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+
 export default api;
