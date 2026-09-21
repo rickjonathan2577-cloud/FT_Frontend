b40187683c0efb7be20898363cd67a1cf37f629f
Rick-jo07 <rickjo.coder@gmail.com>
2026-09-21 08:14:42 +0700

    Token Fixing

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -11,7 +11,7 @@ api.interceptors.request.use((config) => {
   const token = match ? match[2] : null;
 
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.authorization = `Bearer ${token}`;
   }
   return config;
 }, (error) => {
